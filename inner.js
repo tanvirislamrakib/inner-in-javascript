bc0e70c1878b057b557b9577875cc3a9ed017642
tanvirislamrakib <tanvirislamrakib93@gmail.com>
2024-01-13 11:15:03 +0600

    Inner function

---
+++ inner.js
@@ -0,0 +1,11 @@
+
+function world(){
+    var MyName = "tanvir";
+    function bangladesh(){
+    console.log(MyName);
+        let YourName = "Hablu";
+        console.log(YourName);
+    }
+    bangladesh();
+}
+world();
